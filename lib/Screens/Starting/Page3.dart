5b05ca191acf3d50c3904177bbb8ac7b87dd2c8a
kawsar_ahmmed_hridoy <kawsarhridoy0146@gmail.com>
2024-12-17 13:41:10 +0600

    made changes in Page3

--- lib/Screens/Starting/Page3.dart
+++ lib/Screens/Starting/Page3.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:shopping_site/Screens/Signup/SignupScreen.dart';
+import 'package:shopping_site/Screens/Signup/SigninScreen.dart';
 
 class Page3 extends StatelessWidget {
   @override
@@ -9,41 +9,25 @@ class Page3 extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Image.asset('assets/Initial/payment.png', height: 100),
-            Text(
-              'Payment is easy',
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-            ),
-            Text('Secure and fast payment options available.'),
+            SizedBox(height: 50), Image.asset("assets/Initial/payment.png", height: 100), SizedBox(height: 20),
+            Text("Payment is easy", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),),
+            Text("Secure and fast payment options available."), SizedBox(height: 50),
+            Row(mainAxisAlignment: MainAxisAlignment.center, children: [_buildDot(false), _buildDot(false), _buildDot(true),],),
           ],
         ),
       ),
       bottomNavigationBar: Padding(
         padding: const EdgeInsets.all(16.0),
         child: ElevatedButton(
-          onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => SignupScreen()),
-            );
-          },
-          style: ElevatedButton.styleFrom(
-            backgroundColor: Colors.brown,
-            padding: EdgeInsets.symmetric(vertical: 16), // Button height
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(10),
-            ),
-          ),
-          child: Text(
-            'Get Started',
-            style:
-            TextStyle(
-              fontSize: 18,
-              color: Colors.white,
-            ),
-          ),
+          onPressed: () {Navigator.push(context, MaterialPageRoute(builder: (context) => SigninScreen()),);},
+          style: ElevatedButton.styleFrom(backgroundColor: Colors.brown, padding: EdgeInsets.symmetric(vertical: 16), shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10),),),
+          child: Text("Get Started", style: TextStyle(fontSize: 18, color: Colors.white,),),
         ),
       ),
     );
+  }
+
+  Widget _buildDot(bool isActive) {
+    return Container(margin: EdgeInsets.symmetric(horizontal: 5), width: 12, height: 12, decoration: BoxDecoration(color: isActive ? Colors.brown : Colors.grey[300], shape: BoxShape.circle,),);
   }
 }
