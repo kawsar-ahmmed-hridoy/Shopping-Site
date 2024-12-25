310f3c8b6693efbaf0a012ea1b495a5586384ecb
kawsar_ahmmed_hridoy <kawsarhridoy0146@gmail.com>
2024-12-25 20:14:47 +0600

    made changes in Sign In page.

--- lib/Screens/Signup/SigninScreen.dart
+++ lib/Screens/Signup/SigninScreen.dart
@@ -6,49 +6,57 @@ class SigninScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Sign In"), backgroundColor: Colors.white,),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            TextField(decoration: InputDecoration(labelText: "Email", border: OutlineInputBorder(),),),
-            SizedBox(height: 16),
-            TextField(obscureText: true, decoration: InputDecoration(labelText: "Password", border: OutlineInputBorder(),),),
-            SizedBox(height: 8),
-            Align(alignment: Alignment.centerRight, child: TextButton(onPressed: () {}, child: Text("Forget Password?"),),),
-            SizedBox(height: 16),
-            SizedBox(
-              width: 120,
-              child: ElevatedButton(
-                onPressed: () {Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => HomePage()),);},
-                style: ElevatedButton.styleFrom(backgroundColor: Colors.blue, padding: EdgeInsets.symmetric(vertical: 16), shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10),),),
-                child: Text("Sign In", style: TextStyle(fontSize: 18, color: Colors.white,),),
+      backgroundColor: Color(0xFFFFE3E3),
+      body: SingleChildScrollView(
+        child: Container(
+          height: MediaQuery.of(context).size.height,
+          child: Stack(
+            children: [
+              Column(
+                children: [
+                  Expanded(flex: 1, child: Container(margin: EdgeInsets.only(bottom: 50), color: Color(0xFFFFE3E3), child: Center(child: Image.asset("assets/additionals/shop.png", width: 220, height: 150,),),),),
+                  Expanded(flex: 1, child: Container(color: Color(0xFFC9E9D2)),),
+                ],
               ),
-            ),
-            SizedBox(height: 16),
-            Text("Or sign in with"),
-            SizedBox(height: 8),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                SizedBox(width: 50, height: 50, child: IconButton(icon: Image.asset("assets/icons/google.png", fit: BoxFit.contain), onPressed: () {/* google er pore korbo*/},),),
-                SizedBox(width: 50, height: 50, child: IconButton(icon: Image.asset("assets/icons/facebook.png", fit: BoxFit.contain), onPressed: () {/*Facebook er pore krbo*/},),),
-                SizedBox(width: 50, height: 50, child: IconButton(icon: Image.asset("assets/icons/x.png", fit: BoxFit.contain), onPressed: () {/*Twitter er tao pore krbo*/},),),
-              ],
-            ),
-            SizedBox(height: 16),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text("Not have an account?"),
-                TextButton(
-                  onPressed: () {Navigator.push(context, MaterialPageRoute(builder: (context) => SignupScreen()),);},
-                  child: Text("Signup!", style: TextStyle(color: Colors.blue),),
+              Center(
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Container(
+                      padding: EdgeInsets.all(20),
+                      margin: EdgeInsets.only(top: 150,left: 20,right: 20,bottom: 20),
+                      decoration: BoxDecoration(color: Colors.white, borderRadius: BorderRadius.circular(20), boxShadow: [BoxShadow(color: Colors.black26, blurRadius: 8, offset: Offset(0, 4),),],),
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Text("Welcome to Online Shopping!", style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold, color: Color(0xFF213555),),), SizedBox(height: 14),
+                          TextField(decoration: InputDecoration(labelText: "Email", border: OutlineInputBorder(),), style: TextStyle(fontSize: 12),), SizedBox(height: 10),
+                          TextField(obscureText: true, decoration: InputDecoration(labelText: "Password", border: OutlineInputBorder(),), style: TextStyle(fontSize: 12),), SizedBox(height: 10),
+                          ElevatedButton(onPressed: () {Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => HomePage()),);},
+                            style: ElevatedButton.styleFrom(backgroundColor: Colors.blue, shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30),), padding: EdgeInsets.symmetric(horizontal: 40, vertical: 15,),),
+                            child: Text("Sign In", style: TextStyle(fontSize: 15, color: Colors.white,),),
+                          ),
+                        ],
+                      ),
+                    ),
+                    // Forgot Password link
+                    Align(alignment: Alignment.centerRight, child: Padding(padding: EdgeInsets.only(top: 1, right: 50), child: TextButton(onPressed: () {/*future e krbo*/}, child: Text("Forgot Password?", style: TextStyle(color: Colors.blue),),),),), SizedBox(height: 15),
+                    Text("Or sign in with"), SizedBox(height: 8),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        SizedBox(width: 40, height: 40, child: IconButton(icon: Image.asset("assets/icons/google.png", fit: BoxFit.contain,), onPressed: () {/*future e krbo*/},),),
+                        SizedBox(width: 40, height: 40, child: IconButton(icon: Image.asset("assets/icons/facebook.png", fit: BoxFit.contain,), onPressed: () {/*future e krbo*/},),),
+                        SizedBox(width: 40, height: 40, child: IconButton(icon: Image.asset("assets/icons/x.png", fit: BoxFit.contain,), onPressed: () {/*future e krbo*/},),),
+                      ],
+                    ),
+                    SizedBox(height: 20),
+                    Row(mainAxisAlignment: MainAxisAlignment.center, children: [Text("Not have an account?"), TextButton(onPressed: () {Navigator.push(context, MaterialPageRoute(builder: (context) => SignupScreen(),),);}, child: Text("Signup!", style: TextStyle(color: Colors.blue),),),],),
+                  ],
                 ),
-              ],
-            ),
-          ],
+              ),
+            ],
+          ),
         ),
       ),
     );
